52c1398175ce592817f136eda79526542522c587
Hanif <cookiecraft1145@gmail.com>
2025-01-19 11:05:02 +0700

    home stan list getAllFoodItems

--- lib/pages/stan/home_stan_page.dart
+++ lib/pages/stan/home_stan_page.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_canteen/components/my_food_tile.dart';
+import 'package:flutter_application_canteen/models/food.dart';
+import 'package:flutter_application_canteen/models/restaurant.dart';
+import 'package:flutter_application_canteen/pages/customer/food_page.dart';
+import 'package:provider/provider.dart';
 
-class HomeStanPage extends StatelessWidget {
+class HomeStanPage extends StatefulWidget {
   const HomeStanPage({super.key});
+
+  @override
+  State<HomeStanPage> createState() => _HomeStanPageState();
+}
+
+class _HomeStanPageState extends State<HomeStanPage> {
+  // menampilkan semua menu yang ada
+  List<Widget> getAllFoodItems(List<Food> fullMenu) {
+    return [
+      ListView.builder(
+        itemCount: fullMenu.length,
+        physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          final food = fullMenu[index];
+          return FoodTile(
+            food: food,
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => FoodPage(food: food),
+              ),
+            ),
+          );
+        },
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
