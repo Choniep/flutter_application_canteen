a32d5765008f46c74051d4a8d67912caf41c72bf
Hanif <cookiecraft1145@gmail.com>
2025-01-19 18:56:50 +0700

    start designing the profil_stan_page.dart

--- lib/pages/stan/profil_stan_page.dart
+++ lib/pages/stan/profil_stan_page.dart
@@ -5,8 +5,55 @@ class ProfilStanPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profil Page"),
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: const Center(
+          child: Padding(
+            padding: EdgeInsets.only(left: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    ClipOval(
+                      child: Image(
+                        image: AssetImage(
+                          'lib/images/burgers/beef_burger.webp',
+                        ),
+                        width: 130,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Kantin",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          "Pak Lebah",
+                          style: TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Sikma"),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
